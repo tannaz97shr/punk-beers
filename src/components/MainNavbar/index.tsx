7b12f83c8459cd90ebe7098da0971d6f300a7a10
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-01-16 18:21:16 +0300

    feat: create navbar

---
+++ src/components/MainNavbar/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Logo from "../UI/Logo";
+import NavbarList from "./navbarList";
+
+export default function MainHeader() {
+  return (
+    <div className="relative flex items-center w-full px-4 pt-6 border-b border-border-brown text-text-primary">
+      <Link href="/">
+        <Logo />
+      </Link>
+      <NavbarList />
+    </div>
+  );
+}
---
+++ src/models/general.ts
@@ -0,0 +1,6 @@
+export interface INavbarItem {
+  id: string;
+  title: string;
+  href: string;
+  icon?: React.ReactNode;
+}
