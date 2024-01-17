0f66cad85e31631fb4388ae1a5cb56b8f54f4985
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-01-17 19:34:37 +0300

    feat: add description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,17 @@ export default async function Home({
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="z-10 pt-4 max-w-5xl w-full justify-between font-mono text-sm lg:flex flex-col">
-        <h1 className="mt-6 font-bold text-2xl md:text-3xl text-text-primary">
+        <h1 className="mt-6 mb-4 font-bold text-2xl md:text-3xl text-text-primary">
           Browse All Beers
         </h1>
+        <p>
+          Indulge your taste buds in a symphony of flavors with our curated
+          selection of craft beers. From bold IPAs to rich stouts, explore a
+          spectrum of ABV (Alcohol By Volume) levels, each bottle telling a
+          unique story of craftsmanship. Elevate your beer experience with our
+          diverse collection, where every sip is a journey into the world of
+          exceptional brews.
+        </p>
         <Tabs />
         <BeerCardsContainer beers={beers} />
         <Pagination
