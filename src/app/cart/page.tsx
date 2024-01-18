4a860792d7442cd6d107f10396287562c989634a
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-01-18 19:21:35 +0300

    feat: cookie bug

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,22 @@
+import { BeerCardsContainer } from "@/components/BeersSection/BeerCardsContainer";
+import SectionTitle from "@/components/SectionTitle";
+import { IBeer } from "@/models/general";
+import { cookies } from "next/headers";
+
 export default function CartPage() {
+  const cookieStore = cookies();
+  const itemsCookies = cookieStore.get("cart");
+  const itemsArray: IBeer[] = itemsCookies?.value
+    ? JSON.parse(itemsCookies?.value)
+    : [];
+  const sum = itemsArray.reduce((a, { srm }) => a + srm, 0);
+
+  console.log("cart items", itemsArray);
   return (
     <>
-      <div>This is cart page</div>
+      <SectionTitle>Shopping Cart</SectionTitle>
+      <div>Sum of cart : {sum + "$"}</div>
+      <BeerCardsContainer beers={itemsArray} />
     </>
   );
 }
